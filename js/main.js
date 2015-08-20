92dea7e44ea24545cf3fdd49e8abe2c54842a464
cas <casska@gmail.com>
2015-08-19 21:40:22 -0500

    add interval to check if everything is loaded before playing

--- js/main.js
+++ js/main.js
@@ -62,6 +62,8 @@ var gui;
 
 var musicNoise;
 
+var threeReady = false;
+
 function detectSpecs() {
 
   //init HTML elements
@@ -149,6 +151,7 @@ function init() {
     title.style.display = 'inline';
     container.style.display = 'inline';
     gui.domElement.style.display = 'inline';
+    threeReady = true;
   }, function(error) {
     prompt.innerHTML = 'Unable to capture WebCam. Please reload the page.';
   });
@@ -167,9 +170,9 @@ function init() {
   });
 
   /*
-  var mirror = new THREE.Mesh(geometry, meshMaterial);
-  world3D.add(mirror);
-  */
+     var mirror = new THREE.Mesh(geometry, meshMaterial);
+     world3D.add(mirror);
+     */
 
   //add wireframe plane
   wireMaterial = new THREE.MeshBasicMaterial({
@@ -182,106 +185,6 @@ function init() {
   var wiremirror = new THREE.Mesh(geometry, wireMaterial);
   world3D.add(wiremirror);
   wiremirror.position.z = 9;
-
-  /* ************************* */
-  /*
-  var w = window.innerWidth;
-  var h = window.innerHeight;
-
-  planeGeometry = new THREE.PlaneBufferGeometry(w,h);
-
-  scene1 = new THREE.Scene();
-  rt1 = new THREE.WebGLRenderTarget(w, h, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter, format: THREE.RGBFormat });
-  material1 = new THREE.ShaderMaterial({
-    uniforms: {
-      time: { type: 'f' , value: time},
-      resolution: {type: 'v2', value: new THREE.Vector2(w,h)},
-      texture: {type: 't', value: video},
-      mouseX: {type: 'f', value: mouseX},
-      mouseY: {type: 'f', value: mouseY}
-    },
-    vertexShader: document.getElementById("vs").textContent,
-    fragmentShader: document.getElementById("blurFrag").textContent
-  });
-  mesh1 = new THREE.Mesh(planeGeometry, material1);
-  mesh1.position.set(0, 0, 0);
-  scene1.add(mesh1);
-
-  scene2 = new THREE.Scene();
-  rt2 = new THREE.WebGLRenderTarget(w, h, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter, format: THREE.RGBFormat });
-  material2 = new THREE.ShaderMaterial({
-    uniforms: {
-      time: { type: 'f' , value: time},
-      resolution: {type: 'v2', value: new THREE.Vector2(w,h)},
-      texture: {type: 't', value: rt1},
-      texture2: {type: 't', value: video}
-    },
-    //vertexShader: document.getElementById("vs").textContent,
-    //fragmentShader: document.getElementById("fs").textContent
-  });
-  mesh2 = new THREE.Mesh(planeGeometry, material2);
-  mesh2.position.set(0, 0, 0);
-  scene2.add(mesh2);
-  */
-
- /*
-  sceneDiff = new THREE.Scene();
-  rtDiff = new THREE.WebGLRenderTarget(w, h, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter, format: THREE.RGBFormat });
-  materialDiff = new THREE.ShaderMaterial({
-    uniforms: {
-      time: { type: 'f' , value: time},
-      resolution: {type: 'v2', value: new THREE.Vector2(w,h)},
-      texture: {type: 't', value: rt1},
-      texture2: {type: 't', value: rt2},
-      texture3: {type: 't', value: video} 
-    },
-    vertexShader: document.getElementById("vs").textContent,
-    fragmentShader: document.getElementById("diffFs").textContent
-  });
-  meshDiff = new THREE.Mesh(planeGeometry, materialDiff);
-  sceneDiff.add(meshDiff);
-  */
-
-  /*
-  sceneFB = new THREE.Scene();
-  rtFB = new THREE.WebGLRenderTarget(w, h, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter, format: THREE.RGBFormat });
-  materialFB = new THREE.ShaderMaterial({
-    uniforms: {
-      time: { type: 'f' , value: time},
-      resolution: {type: 'v2', value: new THREE.Vector2(w,h)},
-      texture: {type: 't', value: rtDiff},
-      mouseX: {type: 'f', value: mouseX},
-      mouseY: {type: 'f', value: mouseY}
-    },
-    vertexShader: document.getElementById("vs").textContent,
-    fragmentShader: document.getElementById("chromaFs").textContent
-  });
-  meshFB = new THREE.Mesh(planeGeometry, materialFB);
-  sceneFB.add(meshFB);
-
-  sceneFB2 = new THREE.Scene();
-  rtFB2 = new THREE.WebGLRenderTarget(w, h, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter, format: THREE.RGBFormat });
-  materialFB2 = new THREE.ShaderMaterial({
-    uniforms: {
-      time: { type: 'f' , value: time},
-      resolution: {type: 'v2', value: new THREE.Vector2(w,h)},
-      texture: {type: 't', value: rtFB},
-      mouseX: {type: 'f', value: mouseX},
-      mouseY: {type: 'f', value: mouseY}
-    },
-    vertexShader: document.getElementById("vs").textContent,
-    fragmentShader: document.getElementById("flowFs").textContent
-  });
-  meshFB2 = new THREE.Mesh(planeGeometry, materialFB2);
-  sceneFB2.add(meshFB2);
-
-  material = new THREE.MeshBasicMaterial({map: rtFB2});
-  mesh = new THREE.Mesh(planeGeometry, material);
-  scene.add(mesh);
- */
-
-  /* ************************* */
-
 
 
   //init renderer
@@ -328,16 +231,16 @@ function init() {
 // params for dat.gui
 
 function WCMParams() {
-  this.zoom = 1;
+  this.zoom = 1.1;
   this.mOpac = 0;
   this.wfOpac = 0.2;
   this.contrast = 2;
   this.saturation = 1.1;
   this.invertZ = true;
-  this.zDepth = 140;
+  this.zDepth = 100;
   this.noiseStrength = 91;
   this.noiseScale = 0.01;
-  this.noiseSpeed = 0.02;
+  this.noiseSpeed = 0.0002;
   //this.doSnapshot = function() {};
 }
 
@@ -350,6 +253,18 @@ function onParamsChange() {
 function getZDepths() {
 
   noisePosn += params.noiseSpeed;
+  var noiseData = arpPlayer.getAnalyserData();
+  var noiseBass = noiseData[2] / 64.0;
+  noiseBass += noiseData[3] / 64.0;
+
+  var noiseMed = noiseData[8] / 128.0;
+  var noiseHigh = noiseData[11] / 128.0;
+
+  var noiseScale = params.noiseScale + ( noiseBass / 10000 );
+
+  var noiseStrength = params.noiseStrength;
+
+  var zDepth = params.zDepth * noiseHigh;
 
   //draw webcam video pixels to canvas for pixel analysis
   //double up on last pixel get because there is one more vert than pixels
@@ -360,10 +275,13 @@ function getZDepths() {
     for (var j = 0; j < canvasHeight + 1; j++) {
       var color = new THREE.Color(getColor(i, j));
       var brightness = getBrightness(color);
-      var gotoZ = params.zDepth * brightness - params.zDepth / 1.3;
+      var gotoZ = zDepth * brightness - zDepth / 1.3;
 
-      //add noise wobble
-      gotoZ += perlin.noise(i * params.noiseScale, j * params.noiseScale, noisePosn) * params.noiseStrength;
+      //add 32 wobble
+      gotoZ += perlin.noise(i * noiseScale, j * noiseScale, noiseMed) * noiseStrength;
+      gotoZ = gotoZ * noiseHigh;
+      //gotoZ += perlin.noise(i * params.noiseScale, j * params.noiseScale, noiseBass) * params.noiseStrength;
+      //gotoZ += perlin.noise(i * params.noiseScale, j * params.noiseScale, noisePosn) * params.noiseStrength;
       //invert?
       if (params.invertZ) gotoZ = -gotoZ;
       //tween to stablize
@@ -383,6 +301,7 @@ function animate() {
     videoTexture.needsUpdate = true;
     getZDepths();
   }
+  //console.log( arpPlayer.getAnalyserData() );
   //stats.update();
   requestAnimationFrame(animate);
   render();
@@ -473,7 +392,10 @@ CLIENT_ID = '1af5bd17adc32f47529ce064b5e03361';
 var arpPlayer = {
   audioContext: '',
   sourceNode: '',
+  analyserNode: null,
+  dataArray: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   soundBuffer: null,
+  isReady: false,
   isPlaying: false,
   init: function() {
     l('init');
@@ -490,15 +412,15 @@ var arpPlayer = {
     // Create a sound source
     _this.sourceNode = _this.audioContext.createBufferSource();
 
-    // Set playbak rate to pithc down to 443Hz
-    _this.sourceNode.playbackRate.value = 0.981818182;
 
+    // Create a analyser node
+    _this.analyserNode = _this.audioContext.createAnalyser();
+    _this.analyserNode.fftSize = 32;
+    _this.dataArray = new Uint8Array(_this.analyserNode.frequencyBinCount);
 
     // Connect source with audio destination
-    _this.sourceNode.connect(_this.audioContext.destination);       // connect the source to the context's destination (the speakers)
-
-    // Get track url from input
-    var trackUrl = "https://soundcloud.com/a-rp/dreadful-birds-fat-paranoia";//document.getElementById(urlInputId).value;
+    _this.sourceNode.connect(_this.analyserNode);       // connect the source to the context's destination (the speakers)
+    _this.analyserNode.connect(_this.audioContext.destination);       // connect the source to the context's destination (the speakers)
 
     // Initialize Soundcloud SDK
     SC.initialize({
@@ -507,7 +429,7 @@ var arpPlayer = {
 
     // Get track id from Soundcloud
     SC.get('/resolve', {
-      url:'https://soundcloud.com/a-rp/dreadful-birds-fat-paranoia',
+      url:'https://soundcloud.com/a-rp/i',
     }, function(track) {
 
       // Set stream URL as player source
@@ -521,7 +443,8 @@ var arpPlayer = {
         _this.audioContext.decodeAudioData(request.response, function(buffer) {
           _this.soundBuffer = buffer;
           _this.sourceNode.buffer = _this.soundBuffer;                    // tell the source which sound to play
-          _this.play();
+          _this.isReady = true;
+          //_this.play();
         }, function(err) {
           console.log(err);
         });
@@ -544,12 +467,26 @@ var arpPlayer = {
 
     _this.sourceNode.stop(0);  
     _this.isPlaying = 0;
-  }
+  },
+  getAnalyserData: function() {
+    var _this = this;
+
+    _this.analyserNode.getByteTimeDomainData(_this.dataArray);
+    return _this.dataArray;
+  },
 };
 
 $(document).ready(function () {
   'use strict';
   arpPlayer.init();
+
+  // Set interval checker
+  var intervalId = setInterval( function() {
+    if( arpPlayer.isReady == true && threeReady == true ) {
+      arpPlayer.play();
+      clearInterval( intervalId );
+    }
+  }, 300);
 });
 
 $(window).load(function () {
